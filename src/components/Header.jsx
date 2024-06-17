9d288c1f86906568fb6bd709a826e16231175137
hjeffs <hjeffs30+GitHub@gmail.com>
2024-06-17 21:25:59 +0100

    Completes 04-get-all-articles

---
+++ src/components/Header.jsx
@@ -0,0 +1,7 @@
+function Header() {
+    return (
+        <h1>NC NEWS</h1>
+    )
+}
+
+export default Header;
---
+++ src/components/Search.jsx
@@ -0,0 +1,10 @@
+function Search() {
+    return (
+        <form>
+            <input id="searchTerm"/>
+            <button>SEARCH</button>
+        </form>
+    )
+}
+
+export default Search;
